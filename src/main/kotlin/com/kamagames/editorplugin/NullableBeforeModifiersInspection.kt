f73c0712514cc279420d4f2084a2ebadf5ebf34b
Оноприенко Дмитрий <d.onoprienko@funflow.ru>
2024-10-07 17:51:44 +0300

    CS-6676 Поправил работу с void-методами и проверку наличия аннотации

--- src/main/kotlin/com/kamagames/editorplugin/NullableBeforeModifiersInspection.kt
+++ src/main/kotlin/com/kamagames/editorplugin/NullableBeforeModifiersInspection.kt
@@ -8,6 +8,7 @@ import com.intellij.psi.*
 import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.psi.codeStyle.JavaCodeStyleManager
 import com.intellij.refactoring.suggested.createSmartPointer
+import com.kamagames.editorplugin.Annotations.Companion.JAKARTA_NULLABLE
 import com.kamagames.editorplugin.Annotations.Companion.JAVAX_NULLABLE
 
 private const val PROBLEM_DESCRIPTION = "@Nullable should be placed before returning type"
@@ -15,14 +16,20 @@ private const val PROBLEM_DESCRIPTION = "@Nullable should be placed before retur
 
 class NullableBeforeModifiersInspection : AbstractBaseJavaLocalInspectionTool() {
 
+    private fun isNullablePlacedCorrectly(method: PsiMethod, nullableAnnotation: PsiAnnotation): Boolean {
+        return method.typeParameters.isEmpty()
+                && method.modifierList.lastChild.equals(nullableAnnotation)
+                || method.returnTypeElement?.hasAnnotation(nullableAnnotation.qualifiedName!!) ?: false
+    }
+
     override fun checkMethod(
         method: PsiMethod,
         manager: InspectionManager,
         isOnTheFly: Boolean
     ): Array<ProblemDescriptor> {
         val problemsHolder = ProblemsHolder(manager, method.containingFile, isOnTheFly)
-        val nullableAnnotation = AnnotationUtil.findAnnotation(method, JAVAX_NULLABLE)
-        if (nullableAnnotation != null && method.modifierList.lastChild != nullableAnnotation) {
+        val nullableAnnotation = findNullableAnnotation(method)
+        if (nullableAnnotation != null && !isNullablePlacedCorrectly(method, nullableAnnotation)) {
             problemsHolder.registerProblem(
                 method,
                 PROBLEM_DESCRIPTION,
@@ -30,6 +37,11 @@ class NullableBeforeModifiersInspection : AbstractBaseJavaLocalInspectionTool() 
             )
         }
         return problemsHolder.resultsArray
+    }
+
+    private fun findNullableAnnotation(method: PsiMethod): PsiAnnotation? {
+        val jakartaNullable = AnnotationUtil.findAnnotation(method, JAKARTA_NULLABLE)
+        return AnnotationUtil.findAnnotation(method, JAVAX_NULLABLE) ?: jakartaNullable
     }
 }
 
@@ -52,8 +64,11 @@ class PutNullableOnTheReturnTypeFix(
             "Put_@Nullable_on_the_return_type",
             "nullable_annotation_fixes",
             {
-                nullableAnnotationPointer.element!!.delete()
-                val addedNullableAnnotation = method.returnTypeElement?.addAnnotation(JAVAX_NULLABLE)!!
+                val nullableAnnotation = nullableAnnotationPointer.element!!
+                val nullableAnnotationName = nullableAnnotation.qualifiedName!!
+                nullableAnnotation.delete()
+                val addedNullableAnnotation =
+                    method.returnTypeElement?.addAnnotation(nullableAnnotationName)!!
                 JavaCodeStyleManager.getInstance(project).shortenClassReferences(addedNullableAnnotation)
                 CodeStyleManager.getInstance(project).reformat(method.containingFile)
             },
